4e39f67b053407ef49f77e1e6bc1337ba2631ca5
kangraemin <remin1994@gmail.com>
2020-10-06 00:20:33 +0900

    Add zappa api endpoint to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-ALLOWED_HOSTS = []
-
 DEBUG = bool(os.environ.get("DEBUG"))
 
+if not DEBUG:
+    ALLOWED_HOSTS = ["z7qj8d5505.execute-api.ap-northeast-2.amazonaws.com"]
+    
 # Application definition
 
 INSTALLED_APPS = [
