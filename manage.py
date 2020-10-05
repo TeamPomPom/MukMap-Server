a99df6863b32f2abec2bb7826db899d541311e0a
kangraemin <remin1994@gmail.com>
2020-10-06 00:15:51 +0900

    Use dotenv for environment variables in local

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
@@ -17,5 +18,6 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    dotenv.read_dotenv()
     main()
