dfc1939e253583132e6ce23fd67c804a591dad73
kangraemin <remin1994@gmail.com>
2020-10-09 20:22:00 +0900

    Add sentry settings for receive error message

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# For sentry settings 
+if not DEBUG:
+    sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
