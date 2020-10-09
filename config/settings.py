7daa454e6e85e9c369d869fcb258681b0d409078
kangraemin <remin1994@gmail.com>
2020-10-09 21:46:43 +0900

    Use independent s3 storage with zappa s3 for meok-test api

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ if not SERVICE:
     STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "zappa-meok-test"
+    AWS_STORAGE_BUCKET_NAME = "meok-test"
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
