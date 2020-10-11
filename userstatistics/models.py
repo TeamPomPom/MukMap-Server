317f61f816912e8a253b1f3dd623b5cc70f30db5
kangraemin <remin1994@gmail.com>
2020-10-11 22:02:31 +0900

    Fix improper import in user statistics models

--- userstatistics/models.py
+++ userstatistics/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core import models as core_models
 
 
 class UserClickStatistics(core_models.TimeStampedModel):
-    user = models.ForignKey("User", on_delete=models.CASCADE)
+    user = models.ForignKey("users.User", on_delete=models.CASCADE)
     video = models.ForignKey("videos.YoutubeVideos", on_delete=models.CASCADE)
     click_count = models.IntegerField(default=0)
