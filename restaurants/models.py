c3fff99a97484434ddb0bb3a47a43e62074638e5
kangraemin <remin1994@gmail.com>
2020-10-11 18:36:52 +0900

    Add Restaurants default models

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core import models
 
-# Create your models here.
+
+class Restaurants(models.TimeStampedModel):
+
+    name = models.CharField(max_length=100)
+    lat = models.DecimalField(max_digits=10, decimal_places=6)
+    lng = models.DecimalField(max_digits=10, decimal_places=6)
+    district = models.CharField(max_length=100)
