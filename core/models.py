1cf1c904cf11e92101843c89b05cbfd6f90369fe
kangraemin <remin1994@gmail.com>
2020-10-11 21:34:14 +0900

    Add deleted time to DateTimeFields

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True)
 
     class Meta:
         abstract = True
