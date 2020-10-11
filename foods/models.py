4a3e1be1c8b20fd33f5e37026680dbe5fbc30a95
kangraemin <remin1994@gmail.com>
2020-10-11 19:02:45 +0900

    Add FoodCategory default models

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+class FoodCategory(core_models.TimeStampedModel):
+
+    """ Default food category """
+
+    name = models.CharField(max_length=50, blank=True)
+    categoryCode = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class MainFoodCategory(FoodCategory):
+
+
+class SubFoodCategory(FoodCategory):
