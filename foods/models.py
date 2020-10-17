e8f9826324f711cae95fbe62591bbcdf6f7d041f
kangraemin <remin1994@gmail.com>
2020-10-17 23:44:39 +0900

    Add abstract metadata to base food category - Add plural name of categories

--- foods/models.py
+++ foods/models.py
@@ -7,15 +7,20 @@ class FoodCategory(core_models.TimeStampedModel):
     """ Default food category """
 
     name = models.CharField(max_length=50, blank=True)
-    categoryCode = models.CharField(max_length=20)
+    categoryCode = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
 
 
 class MainFoodCategory(FoodCategory):
-    pass
+    class Meta:
+        verbose_name_plural = "Main Food Categories"
 
 
 class SubFoodCategory(FoodCategory):
-    pass
+    class Meta:
+        verbose_name_plural = "Sub Food Categories"
