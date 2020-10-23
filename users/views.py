b7a4d5a6949f018e7ffa1eaf7c781cff764cb282
kangraemin <remin1994@gmail.com>
2020-10-23 22:12:02 +0900

    Add UserViewSet - Add default permission logic

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAdminUser
+from .permissions import IsOwner
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create" or self.action == "retrieve":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
