b98108227f67e600958f630b9323e7cdd5d703f8
kangraemin <remin1994@gmail.com>
2020-10-23 20:11:20 +0900

    Add relatedYoutubeVideoSerializer to channels serializer to get videos info

--- channels/serializers.py
+++ channels/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import YoutubeChannel
+from videos.serializers import RelatedYoutubeVideoSerializer
 
 
 class YoutubeChannelSerializer(serializers.ModelSerializer):
+
+    youtube_videos = RelatedYoutubeVideoSerializer(read_only=True, many=True)
+
     class Meta:
         model = YoutubeChannel
         exclude = ("user", "status")
