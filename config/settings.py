bb9866694ef00f1bd06106d7e9312a58001a8032
kangraemin <remin1994@gmail.com>
2020-10-23 22:06:28 +0900

    Add JWT authentication - Add logic to get User's pk by decoding JWT

--- config/settings.py
+++ config/settings.py
@@ -176,4 +176,8 @@ if not DEBUG:
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 30,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "config.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
