977e58c49a6df6921194c87c5c1a1b21f34ed37f
kangraemin <remin1994@gmail.com>
2020-10-24 02:02:24 +0900

    Fix improper indent in videos permission

--- videos/permissions.py
+++ videos/permissions.py
@@ -1,6 +1,7 @@
-  from rest_framework.permissions import BasePermission
-  from .models import YoutubeVideo
+from rest_framework.permissions import BasePermission
+from .models import YoutubeVideo
 
-  class IsOwnerOfVideo(BasePermission):
-      def has_object_permission(self, request, view, video):
-          return video.youtube_channel.user == request.user
+
+class IsOwnerOfVideo(BasePermission):
+    def has_object_permission(self, request, view, video):
+        return video.youtube_channel.user == request.user
