6c3838a06810c1c3f047af4e7d807dc387e9550f
kangraemin <remin1994@gmail.com>
2020-10-29 23:48:54 +0900

    Add logic to create device fields

--- devices/views.py
+++ devices/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAdminUser
 from .models import Device
 from .serializers import DeviceSerializer
 
@@ -6,3 +7,14 @@ from .serializers import DeviceSerializer
 class DeviceViewSet(ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    def get_permissions(self):
+        if (
+            self.action == "create"
+            or self.action == "update"
+            or self.action == "partial_update"
+        ):
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
