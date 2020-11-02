5c59714ef3ef9b41a28087815ec01e51c3dbf048
kangraemin <remin1994@gmail.com>
2020-11-03 01:31:55 +0900

    Fix logic to auth to use sns_token #13
    - Add Import authenticate 
    - Remove password params for login
    - Use sns_tokens to authentication

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import jwt
+from django.contrib.auth import authenticate
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -31,24 +32,26 @@ class UserViewSet(ModelViewSet):
     @action(detail=False, methods=["post"])
     def login(self, request):
         username = request.data.get("username")
-        password = request.data.get("password")
         google_id = request.data.get("google_id")
         facebook_id = request.data.get("facebook_id")
         apple_id = request.data.get("apple_id")
 
-        if not username or not password:
+        if not username or not (google_id or facebook_id or apple_id):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        if google_id or facebook_id or apple_id:
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                encoded_jwt = jwt.encode(
-                    {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
-                )
-                return Response(data={"token": encoded_jwt, "pk": user.pk})
-            else:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        if google_id:
+            user = authenticate(username=username, google_id=google_id)
+        elif facebook_id:
+            user = authenticate(username=username, facebook_id=facebook_id)
+        elif apple_id:
+            user = authenticate(username=username, apple_id=apple_id)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt, "pk": user.pk})
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
     @action(detail=True)
     def favorites(self, request, pk):
