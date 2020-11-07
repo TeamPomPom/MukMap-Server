6ef04bebdd694cbe39788072ae6019de3e02a958
kangraemin <remin1994@gmail.com>
2020-11-08 00:04:13 +0900

    Install rest_framework_api_key package for api security #18

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,13 @@ PROJECT_APPS = [
     "videos.apps.VideosConfig",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "rest_framework", "haversine", "drf_yasg"]
+THIRD_PARTY_APPS = [
+    "django_countries",
+    "rest_framework",
+    "haversine",
+    "drf_yasg",
+    "rest_framework_api_key",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
