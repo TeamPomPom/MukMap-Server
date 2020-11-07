7d80da1e70b557e4411acfce73edc0c87a030c50
kangraemin <remin1994@gmail.com>
2020-11-07 14:05:32 +0900

    Add top rating main food category api #17 - Add main food category statistic to channel detail serializer - Add query params for top_n / start_date / end_date

--- channels/serializers.py
+++ channels/serializers.py
@@ -2,6 +2,7 @@ from django.db.models import Q, Count, Sum
 from rest_framework import serializers
 from .models import YoutubeChannel
 from .validates import datetime_validate
+from foods.models import MainFoodCategory
 from restaurants.models import Restaurants
 from restaurants.validates import isProperProvince, convertProvince
 
@@ -21,6 +22,7 @@ class YoutubeChannelSerializer(serializers.ModelSerializer):
 class YoutubeChannelDetailSerializer(serializers.ModelSerializer):
 
     province_stat = serializers.SerializerMethodField()
+    main_food_stat = serializers.SerializerMethodField()
 
     class Meta:
         model = YoutubeChannel
@@ -97,3 +99,79 @@ class YoutubeChannelDetailSerializer(serializers.ModelSerializer):
                 )
         return ratio_result
 
+    def get_main_food_stat(self, channel):
+        request = self.context.get("request")
+        top_n = int(request.query_params.get("top_n", 3))
+        start_date = request.query_params.get("start_date", None)
+        end_date = request.query_params.get("end_date", None)
+
+        if top_n <= 0:
+            raise serializers.ValidationError("top_n must be natural number")
+
+        video_query_set = channel.youtube_videos.values_list("main_food_category")
+        if start_date:
+            video_query_set = video_query_set.filter(
+                youtube_video_published_at__gte=datetime_validate(start_date)
+            )
+        if end_date:
+            video_query_set = video_query_set.filter(
+                Q(youtube_video_published_at__lte=datetime_validate(end_date))
+            )
+        main_food_category_stats = (
+            video_query_set.values("main_food_category")
+            .annotate(count_main_food_category=Count("main_food_category"))
+            .order_by("-count_main_food_category")
+        )
+        total_count = main_food_category_stats.aggregate(
+            Sum("count_main_food_category")
+        )["count_main_food_category__sum"]
+        count_result = []
+        for i in range(0, len(main_food_category_stats)):
+            main_food_category = main_food_category_stats[i]["main_food_category"]
+            dup = 0
+            for j in range(0, len(count_result)):
+                if count_result[j]["main_food_category"] == main_food_category:
+                    count_result[j][
+                        "count_main_food_category"
+                    ] += main_food_category_stats[i]["count_main_food_category"]
+                    dup += 1
+            if dup == 0:
+                count_result.append(main_food_category_stats[i])
+        count_result = sorted(
+            count_result, key=lambda k: -k["count_main_food_category"]
+        )
+
+        ratio_result = []
+        if len(count_result) > top_n:
+            for i in range(0, top_n):
+                ratio_result.append(
+                    {
+                        "main_food_category": MainFoodCategory.objects.get(
+                            id=count_result[i]["main_food_category"]
+                        ).name,
+                        "main_food_category_ratio": (
+                            "{:.2f}".format(
+                                count_result[i]["count_main_food_category"]
+                                * 100
+                                / total_count
+                            )
+                        ),
+                    }
+                )
+        else:
+            for i in range(0, len(count_result)):
+                ratio_result.append(
+                    {
+                        "main_food_category": MainFoodCategory.objects.get(
+                            id=count_result[i]["main_food_category"]
+                        ).name,
+                        "main_food_category_ratio": (
+                            "{:.2f}".format(
+                                count_result[i]["count_main_food_category"]
+                                * 100
+                                / total_count
+                            )
+                        ),
+                    }
+                )
+        return ratio_result
