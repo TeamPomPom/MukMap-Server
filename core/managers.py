e52e01265816d9909342302006741547eaa50fa9
kangraemin <remin1994@gmail.com>
2020-11-08 16:52:54 +0900

    Delete unused function in soft deletion manager

--- core/managers.py
+++ core/managers.py
@@ -14,23 +14,3 @@ class SoftDeleteManager(Manager):
         return SoftDeleteQuerySet(
             model=self.model, using=self._db, hints=self._hints
         ).filter(deleted__isnull=True)
-
-    def all_with_deleted(self, *args, **kwargs):
-        return SoftDeleteQuerySet(
-            model=self.model, using=self._db, hints=self._hints
-        ).filter()
-
-    def deleted_set(self, *args, **kwargs):
-        return SoftDeleteQuerySet(
-            model=self.model, using=self._db, hints=self._hints
-        ).filter(deleted__isnull=False)
-
-    def get(self, *args, **kwargs):
-        """ if a specific record was requested, return it even if it's deleted """
-        return self.all_with_deleted().get(*args, **kwargs)
-
-    def filter(self, *args, **kwargs):
-        """ if pk was specified as a kwarg, return even if it's deleted """
-        if "pk" in kwargs:
-            return self.all_with_deleted().filter(*args, **kwargs)
-        return self.get_queryset().filter(*args, **kwargs)
