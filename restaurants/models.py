8bd2efe562abcd89b8c2eb14be6b4554b8952aa5
kangraemin <remin1994@gmail.com>
2020-11-08 16:56:27 +0900

    Disable blank in restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -7,10 +7,10 @@ class Restaurants(core_models.TimeStampedModel):
     name = models.CharField(max_length=150)
     lat = models.DecimalField(max_digits=20, decimal_places=7)
     lng = models.DecimalField(max_digits=20, decimal_places=7)
-    full_address = models.CharField(max_length=500, blank=True)
-    province = models.CharField(max_length=20, blank=True)
+    full_address = models.CharField(max_length=500)
+    province = models.CharField(max_length=20)
     district = models.CharField(max_length=100)
-    old_district = models.CharField(max_length=50, blank=True)
+    old_district = models.CharField(max_length=50)
 
     class Meta:
         db_table = "restuarant"
