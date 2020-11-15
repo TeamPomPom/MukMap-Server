3714ce0f6358bd1b2c73d746b7305dfc46e24a0f
kangraemin <remin1994@gmail.com>
2020-11-15 20:00:47 +0900

    Add logic to filter video object by channel / restaurant #27

--- videos/views.py
+++ videos/views.py
@@ -30,15 +30,37 @@ class YoutubeViedoeViewSet(APIKeyModelViewSet):
 
     def get_permissions(self):
         permission_classes = self.get_base_permission()
-        if self.action == "retrieve" or self.action == "query_search":
+        if (
+            self.action == "retrieve"
+            or self.action == "query_search"
+            or self.action == "list"
+        ):
             permission_classes += [permissions.AllowAny]
         elif self.action == "create":
             permission_classes += [IsApprovedChannel]
-        elif self.action == "list":
-            permission_classes += [permissions.IsAdminUser]
         else:
             permission_classes += [IsOwnerOfVideo]
         return [permission() for permission in permission_classes]
+
+    def list(self, request):
+        queryset = self.queryset
+        channel_id = request.GET.get("channel_id", None)
+        restaurant_id = request.GET.get("restaurant_id", None)
+
+        if restaurant_id:
+            queryset = queryset.filter(Q(restaurant__id=restaurant_id))
+        elif channel_id:
+            queryset = queryset.filter(Q(youtube_channel__id=channel_id))
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @action(detail=False, methods=["get"])
     @renderer_classes(QuerySearchResultRenderer)
