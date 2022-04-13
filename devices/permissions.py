acfbfc8100558fc9e0ac280aeaaa33ac124fe0c9
kangraemin <remin1994@gmail.com>
2022-04-13 23:28:51 +0900

    [Fix] IsOwner에서 DeviceToken 가져오는 로직 변경 - Put 작업일땐 Post body로 받지만, Get 작업일땐 Get parameter 활용할 수 있도록 변경

--- devices/permissions.py
+++ devices/permissions.py
@@ -3,5 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, device):
-        device_token = request.POST.get("device_token")
+        device_token = request.GET.get("device_token")
+        if not device_token:
+            device_token = request.POST.get("device_token")
         return device.device_token == device_token
